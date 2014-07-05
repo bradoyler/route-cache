7925fe95b91db2ee2c8657a5d38e94f12a16ee57
Brad Oyler <bradoyler@gmail.com>
2014-07-04 22:56:00 -0400

    remove logging

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ module.exports.cacheSeconds = function(ttl) {
     var cache = cachestore.get(req.path);
 
     if (cache) {
-      console.info('routecache hit');
       res.send(cache);
     } else {
       var send = res.send;
       res.send = function(string) {
         var body = string instanceof Buffer ? string.toString() : string;
-        console.info('routecache\'d: ' + req.path, ' length: ' + body.length);
         cachestore.put(req.path, body, ttl);
         send.call(this, body);
       };
