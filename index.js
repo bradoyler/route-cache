042ca1f7f2a4c422d839823045f3e950b7cc21b8
Brad Oyler <bradoyler@gmail.com>
2014-07-16 01:25:49 -0400

    adds support for redis client

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 var Eidetic = require('eidetic');
-var cachestore = new Eidetic({
+var redis = require('redis');
+var localcache = new Eidetic({
   maxSize: 50,
   canPutWhenFull: true
 });
 
-module.exports.cacheSeconds = function(ttl) {
+function RouteCache(port, host, password) {
+  if(host) {
+      this.useRedis = true;
+      var options = {};
+      if (password) {
+        options = {auth_pass: password};
+      }
+      this.client = redis.createClient(port, host, options); 
+  }
+}
+
+RouteCache.prototype.useRedis = false;
+
+RouteCache.prototype.cacheSeconds = function(seconds) {
+  if(this.useRedis) {
+    return this.getRedisCache(seconds);
+  }
+  return this.getCache(seconds);
+};
+
+RouteCache.prototype.getRedisCache = function(ttl) {
+  var self = this;
 
   return function(req, res, next) {
+    var cacheKey ='route_'+ req.url;
 
-    var cache = cachestore.get(req.path);
+    self.client.get(cacheKey, function(err, reply) {
+      if (reply) {
+        res.send(reply);
+      } else {
+        var send = res.send;
+        res.send = function(string) {
+          var body = string instanceof Buffer ? string.toString() : string;
+          self.client.set(cacheKey, body);
+          self.client.expire(cacheKey, ttl);
+          send.call(this, body);
+        };
+        next();
+      }
+    });
+  };
+};
+
+RouteCache.prototype.getCache = function(ttl) {
+  var self = this;
+
+  return function(req, res, next) {
+    var cacheKey ='route_'+ req.url;
+    var cache = localcache.get(cacheKey);
 
     if (cache) {
       res.send(cache);
@@ -16,10 +61,12 @@ module.exports.cacheSeconds = function(ttl) {
       var send = res.send;
       res.send = function(string) {
         var body = string instanceof Buffer ? string.toString() : string;
-        cachestore.put(req.url, body, ttl);
+        localcache.put(cacheKey, body, ttl);
         send.call(this, body);
       };
       next();
     }
-  }
+  };
 };
+
+module.exports = RouteCache;
