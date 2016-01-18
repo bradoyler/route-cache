5d9dd6b21a3ab57123a82b9ea02a9f0816b037f6
Joao Moreno <mail@joaomoreno.com>
2016-01-18 17:30:01 +0100

    also cache res.json

--- index.js
+++ index.js
@@ -13,15 +13,21 @@ module.exports.cacheSeconds = function(ttl) {
     var key = req.originalUrl;
     if (redirects[key]) return res.redirect(redirects[key]);
 
-    var cache = cacheStore.get(key);
-    res.original_send = res.send;
-    res.original_redirect = res.redirect;
+    var value = cacheStore.get(key);
 
-    // returns the value immediately
-    if (cache) {
-      res.send(cache);
+    if (value) {
+      // returns the value immediately
+      if (value.isJson) {
+        res.json(value.body);
+      } else {
+        res.send(value.body);
+      }
       return;
     }
+
+    res.original_send = res.send;
+    res.original_json = res.json;
+    res.original_redirect = res.redirect;
 
     if (!queues[key]) {
       queues[key] = [];
@@ -31,9 +37,13 @@ module.exports.cacheSeconds = function(ttl) {
     if (queues[key].length === 0
       && queues[key].push(function noop(){})) {
 
-      res.send = function (string) {
-        var body = string instanceof Buffer ? string.toString() : string;
-        if (res.statusCode < 400) cacheStore.put(key, body, ttl);
+      var didHandle = false;
+
+      function rawSend(data, isJson) {
+        didHandle = true;
+
+        var body = data instanceof Buffer ? data.toString() : data;
+        if (res.statusCode < 400) cacheStore.put(key, { body: body, isJson: isJson }, ttl);
 
         // drain the queue so anyone else waiting for
         // this value will get their responses.
@@ -43,7 +53,24 @@ module.exports.cacheSeconds = function(ttl) {
             process.nextTick(subscriber);
           }
         }
-        res.original_send(body);
+
+        if (isJson) {
+          res.original_json(body);
+        } else {
+          res.original_send(body);
+        }
+      }
+
+      res.send = function (data) {
+        if (didHandle) {
+          res.original_send(data);
+        } else {
+          rawSend(data, false);
+        }
+      };
+
+      res.json = function (data) {
+        rawSend(data, true);
       };
 
       // If response happens to be a redirect -- store it to redirect all
@@ -58,8 +85,12 @@ module.exports.cacheSeconds = function(ttl) {
       // subsequent requests will batch while the first computes
     } else {
       queues[key].push(function() {
-        var body = cacheStore.get(key);
-        res.send(body);
+        var value = cacheStore.get(key);
+        if (value.isJson) {
+          res.json(value.body);
+        } else {
+          res.send(value.body);
+        }
       });
     }
 
@@ -69,7 +100,7 @@ module.exports.cacheSeconds = function(ttl) {
 
 
 module.exports.removeCache = function(url) {
-  if (redirects[url]) 
+  if (redirects[url])
     delete redirects[url];
   cacheStore.del(url);
 };
