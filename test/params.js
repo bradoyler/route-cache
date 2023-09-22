15076715bd86be03715859fef413d640a46ca1a7
yutak23 <yutapilafu@gmail.com>
2023-09-22 16:58:57 +0900

    feat: status codes are also cached

--- test/params.js
+++ test/params.js
@@ -1,9 +1,11 @@
 'use strict'
 var request = require('supertest'),
   routeCache = require('../index'),
-  express = require('express')
+  express = require('express'),
+  assert = require('assert')
 
 var paramTestIndex = 0
+var noContentIndex = 0
 
 describe('Params / Querystrings', function () {
   var app = express()
@@ -11,6 +13,11 @@ describe('Params / Querystrings', function () {
   app.get('/params', routeCache.cacheSeconds(1, '/params-test'), function (req, res) {
     paramTestIndex++
     res.send('Params test ' + paramTestIndex)
+  })
+
+  app.get('/params-204', routeCache.cacheSeconds(10, '/params-test-204'), function (req, res) {
+    noContentIndex++
+    res.status(204).send()
   })
 
   var agent = request.agent(app)
@@ -34,5 +41,32 @@ describe('Params / Querystrings', function () {
       .get('/params?a=2')
       .expect(200)
       .expect('Params test 1', done)
+  })
+
+  it('204 without params', function (done) {
+    agent
+      .get('/params-204')
+      .expect(204, () => {
+        assert.equal(noContentIndex, 1)
+        done()
+      })
+  })
+
+  it('204 with a=1', function (done) {
+    agent
+      .get('/params-204?a=1')
+      .expect(204, () => {
+        assert.equal(noContentIndex, 1)
+        done()
+      })
+  })
+
+  it('204 with a=2', function (done) {
+    agent
+      .get('/params-204?a=2')
+      .expect(204, () => {
+        assert.equal(noContentIndex, 1)
+        done()
+      })
   })
 })
